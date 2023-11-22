b73fc86791ca4c99cf2c00e001117816a55455fb
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2023-11-22 17:04:57 +0900

    Title: 방향에 맞춰 이동, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231122/방향에 맞춰 이동/move-in-direction.java
+++ 231122/방향에 맞춰 이동/move-in-direction.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
 public class Main {
+    public static int n;
+    public static int[] dx = new int[]{1, -1, 0, 0};
+    public static int[] dy = new int[]{0, 0, -1, 1};
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        char cDir;
+        int dist;
+        int dir = 0;
+        int x = 0;
+        int y = 0;
+        
+        while (n-- > 0) {
+            cDir = sc.next().charAt(0);
+            dist = sc.nextInt();
+
+            switch (cDir) {
+                case 'E' :
+                    dir = 0;
+                    break;
+                case 'W' :
+                    dir = 1;
+                    break;
+                case 'S' :
+                    dir = 2;
+                    break;
+                case 'N' :
+                    dir = 3;
+                    break;
+            }
+
+            x += dx[dir]*dist;
+            y += dy[dir]*dist;
+        }
+
+        System.out.printf("%d %d", x, y);
     }
 }
