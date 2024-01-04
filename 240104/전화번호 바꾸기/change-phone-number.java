ef78b9dc0e188d0fc3e689aa3858c3e7c09be94a
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-04 15:37:43 +0900

    Title: 전화번호 바꾸기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/전화번호 바꾸기/change-phone-number.java
+++ 240104/전화번호 바꾸기/change-phone-number.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // // 방법 (1) String.split("<구분자>") 활용
+        // Scanner sc = new Scanner(System.in);
+
+        // String[] numbers = new String[3];
+
+        // numbers = sc.next().split("-");
+
+        // System.out.printf("%s-%s-%s", numbers[0], numbers[2], numbers[1]);
+
+        // 방법 (2) Scanner.useDelimiter("<구분자>") 활용
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a, b, c;
+        a = sc.next();
+        b = sc.next();
+        c = sc.next();
 
-        String[] numbers = new String[3];
-
-        numbers = sc.next().split("-");
-
-        System.out.printf("%s-%s-%s", numbers[0], numbers[2], numbers[1]);
+        System.out.printf("%s-%s-%s", a, c, b);
     }
 }
