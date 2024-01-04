66b1db55206c73abdfc9b4f038eeed29cc671b06
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-04 15:41:53 +0900

    Title: 합과 차의 나눗셈, Time: 213ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240104/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -4,11 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double a, b;
+        // // 방법 (1) 처음부터 double 로 받기
+        // double a, b;
         
-        a = sc.nextDouble();
-        b = sc.nextDouble(); 
+        // a = sc.nextDouble();
+        // b = sc.nextDouble(); 
 
-        System.out.printf("%.2f", (a+b) / (a-b));
+        // System.out.printf("%.2f", (a+b) / (a-b));
+
+        // 방법 (2) 처음에는 int 로 받고 나중에 double 로 형변환
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.printf("%.2f", (double) (a+b) / (a-b));
     }
 }
