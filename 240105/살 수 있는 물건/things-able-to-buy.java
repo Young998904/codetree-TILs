09b78bd79c8c727642a9dc7ef107624e41aea34c
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-05 23:33:32 +0900

    Title: 살 수 있는 물건, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240105/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int max = 0;
+
+        if (n >= 3000) {
+            System.out.println("book");
+            return;
+        }
+        else if (n >= 1000) {
+            System.out.println("mask");
+            return;
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
