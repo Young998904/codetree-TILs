ae52dd3652688b9d37923e0b8d2040c2e65577be
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-07 15:54:01 +0900

    Title: 홀수만 출력하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240107/홀수만 출력하기/print-only-odd-numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int input;
+        for (int i=1; i<=n; i++) {
+            input = sc.nextInt();
+            if (input % 3 == 0) {
+                System.out.println(input);
+            }
+        }
+    }
+}
