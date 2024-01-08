bc46a840c6ff303f162e7042c816bd3e28c00999
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-08 12:49:37 +0900

    Title: 정사각형 두 개 출력, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240108/정사각형 두 개 출력/output-two-rectangles.java
+++ 240108/정사각형 두 개 출력/output-two-rectangles.java
@@ -1,25 +1,49 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // // 방법 (1) 단순 출력
+        // Scanner sc = new Scanner(System.in);
+
+        // int n = sc.nextInt();
+
+        // for (int i=1; i<=n; i++) {
+        //     for (int j=1; j<=n; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println();
+
+        // for (int i=1; i<=n; i++) {
+        //     for (int j=1; j<=n; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // 방법 (2) : BufferedWriter 사용
         Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = sc.nextInt();
 
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=n; j++) {
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
-
-        System.out.println();
+        bw.newLine();
 
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=n; j++) {
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
+        bw.flush();
     }
 }
