3cee9f0611c2127c90a296bd7656291559c19be1
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-10 23:43:10 +0900

    Title: 알파벳 출력하기 2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/알파벳 출력하기 2/print-alphabet-2.java
+++ 240110/알파벳 출력하기 2/print-alphabet-2.java
@@ -11,10 +11,10 @@ public class Main {
 
         for (int i=0; i<n; i++) {
             for (int j=0; j<i; j++) System.out.print("  ");
-            if (cnt > 90) cnt = 65;
             for (int k=0; k<n-i; k++) {
                 System.out.print((char)cnt + " ");
                 cnt++;
+                if (cnt > 90) cnt = 65;
             }
             System.out.println();
         }
