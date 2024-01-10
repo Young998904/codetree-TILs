ee709d5844dc486fe6494b9cee3888138e6240e5
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-11 00:30:07 +0900

    Title: 두 숫자의 차의 최솟값, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240111/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -12,13 +12,23 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
+        // // 방법(1) : 무식하게 하나하나 해보기
+        // int min = Integer.MAX_VALUE;
+
+        // for (int i=n-1; i>=0; i--) {
+        //     for (int j=0; j<=n; j++) {
+        //         if (j >= i) continue;
+        //         min = Math.min(min, arr[i]-arr[j]);
+        //     }
+        // }
+
+        // System.out.print(min);
+
+        // 방법(2) 생각해서 풀기
         int min = Integer.MAX_VALUE;
 
-        for (int i=n-1; i>=0; i--) {
-            for (int j=0; j<=n; j++) {
-                if (j >= i) continue;
-                min = Math.min(min, arr[i]-arr[j]);
-            }
+        for (int i=1; i<n; i++) {
+            min = Math.min(min, arr[i]-arr[i-1]);
         }
 
         System.out.print(min);
