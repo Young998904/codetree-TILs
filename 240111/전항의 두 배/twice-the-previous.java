34eb5210217182ac3bea018544aaaaec5ab0cddc
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-11 00:03:34 +0900

    Title: 전항의 두 배, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/전항의 두 배/twice-the-previous.java
+++ 240111/전항의 두 배/twice-the-previous.java
@@ -9,6 +9,8 @@ public class Main {
         arr[0] = sc.nextInt();
         arr[1] = sc.nextInt();
 
+        System.out.print(arr[0] + " " + arr[1] + " ");
+
         for (int i=2; i<=9 ; i++) {
             arr[i] = arr[i-1] + 2*arr[i-2];
 
