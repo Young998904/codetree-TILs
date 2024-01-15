19d96f6425c7eef5765c4e8f32819ade033a60d1
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-15 11:18:37 +0900

    Title: 최소공배수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240115/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240115/최소공배수 구하기/find-the-least-common-multiple.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
+    // // 방법 (1) : 내풀이
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     int n = sc.nextInt();
+    //     int m = sc.nextInt();
+
+    //     System.out.print(func(Math.min(n,m), Math.max(n,m)));
+    // }
+    // public static int func (int a, int b) {
+    //     int answer ;
+    //     for (int i=1; ;i++) {
+    //         if (b * i % a == 0) return b*i;
+    //     }
+    // }
+
+    // 방법 (2) LCM(n,m) = (n * m) / GCD(n,m) 사용
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        System.out.print(func(Math.min(n,m), Math.max(n,m)));
+        System.out.print(lcm(n,m));
     }
-    public static int func (int a, int b) {
-        int answer ;
-        for (int i=1; ;i++) {
-            if (b * i % a == 0) return b*i;
+    public static int lcm (int n, int m) {
+        int gcd = 0;
+        
+        for (int i=Math.min(n,m); i>=1; i--) {
+            if (n % i == 0 && m % i == 0) {
+                gcd = i;
+                break;
+            }
         }
+
+        return (n * m) / gcd;
     }
 }
