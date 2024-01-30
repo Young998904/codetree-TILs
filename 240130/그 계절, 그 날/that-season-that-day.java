2889fa1d5efa60df63e5be29cc2a8ccb15148f5d
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2024-01-30 15:10:17 +0900

    Title: 그 계절, 그 날, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/그 계절, 그 날/that-season-that-day.java
+++ 240130/그 계절, 그 날/that-season-that-day.java
@@ -26,8 +26,10 @@ public class Main {
             switch (m) {
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                     maxDay = 31;
+                    break;
                 case 4: case 6: case 9: case 11:
                     maxDay = 30;
+                    break;
             }
 
             boolean isPossible = true;
